021801e9a8c4a716fd14cf72631a9dfd04745331
Thomas Braam <thomas.braam@protonmail.com>
2021-02-14 22:14:35 +0100

    added config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_7xNS9xCWF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '53drd8jelo621ffggk2aa8npnq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
